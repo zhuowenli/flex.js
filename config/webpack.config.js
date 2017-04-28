cb92bcfc131dba50c0eb95a065aca720c60c334b
卓文理 <531840344@qq.com>
2017-04-28 15:35:21 +0800

    :tada: initial commit

---
+++ config/webpack.config.js
@@ -0,0 +1,79 @@
+/*
+ * @Author: 卓文理
+ * @Email: 531840344@qq.com
+ * @Date: 2017-04-28 15:14:38
+ */
+
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const env = process.env.MEIYA_ENV;
+
+module.exports = {
+    entry: {
+        flex: '../flex.js',
+        app: '../example/app.js',
+    },
+    output: {
+        filename: '[name].min.js',
+        path: path.resolve(__dirname, '../dist'),
+    },
+    resolve: {
+        extensions: ['.js', '.json'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env'],
+                    },
+                },
+            },
+            {
+                test: /\.(scss|sass)$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: { minimize: true, importLoaders: true },
+                        },
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                plugins: () => [
+                                    require('autoprefixer')({
+                                        browsers: ['ie > 8', 'last 4 versions'],
+                                        remove: false,
+                                    }),
+                                    require('postcss-pxtorem')({
+                                        rootValue: 32,
+                                        selectorBlackList: ['html'],
+                                        minPixelValue: 1,
+                                        propWhiteList: ['*'],
+                                    }),
+                                ],
+                            },
+                        },
+                        { loader: 'sass-loader', options: { sourceMap: true } },
+                    ],
+                }),
+            },
+            {
+                test: /\.(jpe?g|gif|png|svg)$/i,
+                use: `file-loader?name=${env ? '' : '/dist/'}img/[name].[ext]`,
+            },
+        ],
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin(),
+        new ExtractTextPlugin('[name].min.css'),
+    ],
+};
---
+++ flex.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @export
+ * @param {number} [baseFontSize=100] 基础fontSize, 默认100px;
+ * @param {number} [fontscale=1] 有的业务希望能放大一定比例的字体;
+ */
+export default function flex(baseFontSize = 100, fontscale = 1) {
+    const doc = window.document;
+    const ua = navigator.userAgent;
+    const matches = ua.match(/Android[\S\s]+AppleWebkit\/(\d{3})/i);
+    const UCversion = ua.match(/U3\/((\d+|\.){5,})/i);
+    const isUCHD = UCversion && parseInt(UCversion[1].split('.').join(''), 10) >= 80;
+    const isiOS = navigator.appVersion.match(/(iphone|ipad|ipod)/gi);
+    let dpr = window.devicePixelRatio || 1;
+
+    if (!isiOS && !(matches && matches[1] > 534) && !isUCHD) {
+        // 如果非iOS, 非Android4.3以上, 非UC内核, 就不执行高清, dpr设为1;
+        dpr = 1;
+    }
+
+    const scale = 1 / dpr;
+
+    let metaEl = doc.querySelector('meta[name="viewport"]');
+
+    if (!metaEl) {
+        metaEl = doc.createElement('meta');
+        metaEl.setAttribute('name', 'viewport');
+        doc.head.appendChild(metaEl);
+    }
+
+    metaEl.setAttribute('content', `width=device-width,user-scalable=no,initial-scale=${scale},maximum-scale=${scale},minimum-scale=${scale}`);
+
+    doc.documentElement.style.fontSize = `${(baseFontSize / 2) * dpr * fontscale}px`;
+}
+
+window.flex = flex;
